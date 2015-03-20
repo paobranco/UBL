7c1035986e322ec076ffee9f94a06df98b26bccd
pbranco <paobranco@gmail.com>
2015-03-20 16:50:56 +0000

    random under-sampling classification

--- R/randUnder.R
+++ R/randUnder.R
@@ -5,16 +5,16 @@
 # existing classes or it can "smoothly invert" the frequency
 # of the examples in each class
 # Examples:
-#   ir<- iris[-c(95:130),]
-#   under.iris <- randUnderClassif(Species~., ir, list(setosa=0.5, versicolor=0.8))
-#   balance.iris <- randUnderClassif(Species~., ir, "balance")
-#   invert.iris <- randUnderClassif(Species~., ir, "extreme")
-#   
-#   library(MASS)
-#   data(cats)
-#   under.cats <- randUnderClassif(Sex~., cats, list(M=0.8))
-#   balance.cats <- randUnderClassif(Sex~., cats,"balance")
-#   invert.cats <- randUnderClassif(Sex~., cats, "extreme")
+  ir<- iris[-c(95:130),]
+  under.iris <- randUnderClassif(Species~., ir, list(setosa=0.5, versicolor=0.8))
+  balance.iris <- randUnderClassif(Species~., ir, "balance")
+  invert.iris <- randUnderClassif(Species~., ir, "extreme")
+  
+  library(MASS)
+  data(cats)
+  under.cats <- randUnderClassif(Sex~., cats, list(M=0.8))
+  balance.cats <- randUnderClassif(Sex~., cats,"balance")
+  invert.cats <- randUnderClassif(Sex~., cats, "extreme")
 # P. Branco, Mar 2015
 # ---------------------------------------------------
 randUnderClassif <- function(form, data, C.perc, repl=FALSE)
@@ -39,8 +39,7 @@ randUnderClassif <- function(form, data, C.perc, repl=FALSE)
       stop("percentages provided must be less than 1 to perform under-sampling!")
     }
     names.und <- names(which(C.perc<1))
-    pos <- as.numeric(match(names.und,names))
-    tot.num <-li[[2]][pos]
+
     # include example from classes unchanged
     newdata <- data[which(data[,tgt] %in% names[which(!(names %in% names.und))]),]
   
